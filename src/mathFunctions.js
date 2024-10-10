19111f47e9e587032a592862d8c54bae2e0fcfca
vinicius-vasconcelos <viniciussouzav@gmail.com>
2024-10-10 16:32:57 -0300

    Criando primeiro aquivo de funções matemáticas

---
+++ src/mathFunctions.js
@@ -0,0 +1,6 @@
+const sum = (numA, numB) => numA + numB;
+const sub = (numA, numB) => numA - numB;
+const div = (numA, numB) => numA / numB;
+const mul = (numA, numB) => numA * numB;
+
+export { sum, sub, div, mul };
