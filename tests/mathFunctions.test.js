682a5aa134b5d1b415a3b1793611dd375eeab48a
vinicius-vasconcelos <viniciussouzav@gmail.com>
2024-10-27 22:33:50 -0300

    Deletando a mudando arquivos de teste para fazer na aula

--- tests/mathFunctions.test.js
+++ tests/mathFunctions.test.js
@@ -5,18 +5,3 @@
   - Chai é responsável pelas asserções, e expect é uma das formas de expressá-las. 
     - Com "expect", você verifica se o código está se comportando como esperado.
 */
-
-import { expect } from 'chai';
-import { sum } from '../src/mathFunctions.js';
-
-describe('Teste na função sum:', () => {
-  it('deve retornar a soma de dois números', () => {
-    const result = sum(2, 3);
-    expect(result).to.equal(5);
-  });
-
-  it('deve retornar um número negativo quando a soma for negativa', () => {
-    const result = sum(-2, -3);
-    expect(result).to.equal(-5);
-  });
-});
